b986bf520d2b6af27449d32ad337c116fa4689c6
Maheshwor <shresthamahesh9843@gmail.com>
2023-03-06 18:41:08 +0545

    completed speciality section

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const searchInput = document.getElementsByClassName("field")[0];
 const searchField = document.getElementsByClassName("search-field")[0];
 let isInputFocused = false;
 
-searchInput.addEventListener("focus", () => {
-  isInputFocused = true;
-  if (isInputFocused) {
+function setWidth() {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth >= 768) {
+    searchField.style.width = isInputFocused ? "50%" : "30%";
+  } else {
     searchField.style.width = "50%";
   }
+}
+
+setWidth(); // Call initially to set the width based on the viewport size
+
+window.addEventListener("resize", setWidth); // Call on window resize to update the width
+
+searchInput.addEventListener("focus", () => {
+  isInputFocused = true;
+  setWidth(); // Call to update the width
 });
 
 searchInput.addEventListener("blur", () => {
   isInputFocused = false;
-  if (!isInputFocused) {
-    searchField.style.width = "30%";
-  }
+  setWidth(); // Call to update the width
 });
-
-
-
-
